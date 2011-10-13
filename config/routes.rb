e5404b55cc1e6446425b997b1e769558247fa924
morta <morta@digitus.itk.ppke.hu>
2011-10-14 00:36:20 +0200

    Done with static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ RailsChapter3::Application.routes.draw do
   get "pages/home"
 
   get "pages/contact"
+  
+  get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
